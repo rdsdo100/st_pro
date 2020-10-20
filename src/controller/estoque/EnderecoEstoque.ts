2d01e3c1ebb08e4db434c6650197ceb1063be3f0
Rubens Diego <rdsdo2011@gmail.com>
2020-10-20 12:54:01 -0300

    cadstro de enderecos

--- src/controller/estoque/EnderecoEstoque.ts
+++ src/controller/estoque/EnderecoEstoque.ts
@@ -1,6 +1,12 @@
+import { Controller, Get, Post } from "@overnightjs/core";
 import {Request, Response} from "express";
-interface cadastroEnderecos {
-    estoque?: string,
+import { getConnection, getRepository, Transaction } from "typeorm";
+import { EstoqueEnderecos } from "../../entity/EstoqueEnderecos";
+import { Estoques } from "../../entity/Estoques";
+import Estoque from "./Estoque";
+
+interface IsCadastroEnderecos {
+    estoque: string,
     zonaI: string ,
     zonaF: string ,
     ruaI: string ,
@@ -13,12 +19,39 @@ interface cadastroEnderecos {
 
 }
 
-
+@Controller('estoques-endereco')
 export default class EnderecoEstoque {
+    @Get()
     async index(request: Request , response: Response){
 
+        const enderecoEstoqueRepository = getRepository(EstoqueEnderecos)
+const estoqueEnderecos = new EstoqueEnderecos
 
 
 
     }
+
+@Post()
+    async cadastroEnderecos (request: Request , response: Response){
+
+        const enderecoEstoqueRepository = getRepository(EstoqueEnderecos)
+        const estoqueEnderecos = new EstoqueEnderecos
+        const estoque = new Estoques()
+        
+        
+
+        estoqueEnderecos.zona = '101'
+        estoqueEnderecos.rua = '2'
+        estoqueEnderecos.coluna = '2'
+        estoqueEnderecos.nivel = '2'
+        estoqueEnderecos.ativo = true
+        estoque.id = 1
+        estoqueEnderecos.estoqueIdfK = estoque
+
+        
+       const retorno = await enderecoEstoqueRepository.save(estoqueEnderecos)
+
+
+    }
+
 }
